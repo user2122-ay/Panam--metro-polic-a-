ddbe8f4482d83598954f361abace1455edcf30e1
user2122-ay <anthonybatista2010@gmail.com>
2026-07-06 21:40:27 -0500

    Refactor config.js to add enlaces and clean up

--- config.js
+++ config.js
@@ -1,13 +1,14 @@
 module.exports = {
-    // Canales
     canales: {
         bienvenida: '1523547183608107089',
-        subdivision: '1523547450726551693',
-        funciones: '1523547701109592064',
-        postulacion: '1523548265340076184',
     },
 
-    // Mensajes motivadores para la bienvenida (agrega los que quieras)
+    enlaces: {
+        subdivision: 'https://discord.com/channels/1523544234823520388/1523547450726551693',
+        funciones: 'https://discord.com/channels/1523544234823520388/1523547701109592064',
+        postulacion: 'https://discord.com/channels/1523544234823520388/1523548265340076184',
+    },
+
     mensajesMotivadores: [
         'La disciplina y el honor son la base de nuestra institución. ¡Bienvenido a las filas!',
         'Cada nuevo miembro fortalece nuestra misión de servir y proteger. ¡Éxitos en tu camino!',
@@ -15,8 +16,6 @@ module.exports = {
         'Servir a la comunidad es el mayor honor. Hoy comienza tu historia con nosotros.',
     ],
 
-    // Imagen del embed de bienvenida (pega la URL cuando la tengas)
-    imagenBienvenida: 'PON_AQUI_LA_URL_DE_TU_IMAGEN',
-
-    
+    // Pega la URL de tu imagen aquí cuando la tengas
+    imagenBienvenida: '',
 };
