5cde75f55b1b7fbc8b94b2cdd58a7f05040f9459
user2122-ay <anthonybatista2010@gmail.com>
2026-07-06 20:37:00 -0500

    Initialize Discord bot with dynamic command and event loading

---
+++ index.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const { conectarMongo } = require('./config/database');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ]
+});
+
+client.commands = new Collection();
+
+// Cargar comandos dinámicamente desde /commands
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(path.join(commandsPath, file));
+    client.commands.set(command.data.name, command);
+}
+
+// Cargar eventos dinámicamente desde /events
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+    const event = require(path.join(eventsPath, file));
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args, client));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args, client));
+    }
+}
+
+async function iniciar() {
+    await conectarMongo();
+    await client.login(process.env.DISCORD_TOKEN);
+}
+
+iniciar();
